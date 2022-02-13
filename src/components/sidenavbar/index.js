cf58e1279b65d3e5b27474bcb9e548b50ec2247b
Johny Miro <johny@Air-de-Johny.Home>
2022-02-13 00:11:01 +0000

    sidenavbar 50% finished

--- src/components/sidenavbar/index.js
+++ src/components/sidenavbar/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled, { css } from "styled-components";
+import React, { useState } from "react";
+import styled, { css, useTheme } from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 
 import * as colors from "../../colors";
@@ -9,35 +9,67 @@ import useMediaQuery from "../../hooks/useMediaQuery";
 
 export default function SideNavBar() {
   /* Write the necessary functions to show and hide the side bar on small devices */
-  const isDesktop = useMediaQuery('(min-width: 960px)')
+  const isDesktop = useMediaQuery("(min-width: 960px)");
+  const [selected, setSelected] = useState("");
 
   return (
     <SideNavBarCont className={isDesktop ? "visible" : "hidden"}>
       {/* Implement a hamburger icon slide in effect for small devices */}
-      <SideNavMainLink className="menu_nav_link main_nav_link" to="/" exact>
+      <SideNavMainLink
+        onClick={() => setSelected("wesley")}
+        selected={selected === "wesley"}
+        className="menu_nav_link main_nav_link"
+        to="/"
+        exact
+      >
         Wesley
         <NavIcon arrow></NavIcon>
       </SideNavMainLink>
-      <SideNavMainLink className="menu_nav_link" to="/discover">
+      <SideNavMainLink
+        onClick={() => setSelected("discover")}
+        selected={selected === "discover"}
+        className="menu_nav_link"
+        to="/discover"
+      >
         Discover
         <NavIcon search></NavIcon>
       </SideNavMainLink>
       <SideNavHeader>
         <HeaderText>Watched</HeaderText>
       </SideNavHeader>
-      <NavLink className="menu_nav_link" to="/watched/movies">
+      <NavLink
+        onClick={() => setSelected("watched_movies")}
+        selected={selected === "watched_movies"}
+        className="menu_nav_link"
+        to="/watched/movies"
+      >
         Movies
       </NavLink>
-      <NavLink className="menu_nav_link" to="/watched/tv-shows">
+      <NavLink
+        onClick={() => setSelected("watched_tvShows")}
+        selected={selected === "watched_tvShows"}
+        className="menu_nav_link"
+        to="/watched/tv-shows"
+      >
         Tv Shows
       </NavLink>
       <SideNavHeader>
         <HeaderText>Saved</HeaderText>
       </SideNavHeader>
-      <NavLink className="menu_nav_link" to="/saved/movies">
+      <NavLink
+        onClick={() => setSelected("saved_movies")}
+        selected={selected === "saved_movies"}
+        className="menu_nav_link"
+        to="/saved/movies"
+      >
         Movies
       </NavLink>
-      <NavLink className="menu_nav_link" to="/saved/tv-shows">
+      <NavLink
+        onClick={() => setSelected("saved_tvShows")}
+        selected={selected === "saved_tvShows"}
+        className="menu_nav_link"
+        to="/saved/tv-shows"
+      >
         Tv Shows
       </NavLink>
     </SideNavBarCont>
@@ -50,15 +82,27 @@ const SideNavBarCont = styled.div`
   width: 260px;
   height: 100%;
   background-color: ${colors.sideNavBar};
+  transition: all 200ms ease;
+
+  color: white;
+  box-sizing: border-box;
+  padding-top: 35px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const SideNavMainLink = styled(Link)`
   position: relative;
   display: block;
-  padding: 25px 35px;
   font-size: 1.6em;
-  font-weight: 700;
   color: white;
+  padding: 15px;
+  padding-left: 35px;
+  transition: all 200ms ease;
+  background-color: ${(props) => (props.selected ? colors.primaryColor : "")};
+  &:hover {
+    color: ${(props) => (!props.selected ? colors.primaryColor : "white")};
+  }
 `;
 
 const NavIcon = styled.div`
@@ -67,10 +111,26 @@ const NavIcon = styled.div`
   top: 50%;
 `;
 
-const SideNavHeader = styled.div``;
+const SideNavHeader = styled.div`
+  font-size: 1.6em;
+  margin: 30px 0 15px 0;
+  padding-bottom: 12px;
+  border-bottom: 1px solid grey;
+  padding-left: 35px;
+`;
 
 const HeaderText = styled.div``;
 
 const NavLink = styled(Link)`
   display: block;
+  color: ${(props) => (props.selected ? "white" : "lightgrey")};
+  font-size: 1.4em;
+  font-weight: 300;
+  padding: 10px 0;
+  padding-left: 35px;
+  transition: all 200ms ease;
+  background-color: ${(props) => (props.selected ? colors.primaryColor : "")};
+  &:hover {
+    color: ${(props) => (!props.selected ? colors.primaryColor : "white")};
+  }
 `;
